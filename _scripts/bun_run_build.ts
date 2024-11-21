128daf56762e00d049ececec6b977b39ea6ad98e
Gerold Penz <gerold@halvar.at>
2024-11-21 09:41:33 +0100

    Version in Docker-Images

--- _scripts/bun_run_build.ts
+++ _scripts/bun_run_build.ts
@@ -7,6 +7,14 @@ $.cwd(join(__dirname, ".."))
 
 let exitCode = 1
 try {
+    exitCode = (await $`bun run incr_version`).exitCode
+} finally {
+    if (exitCode !== 0) {
+        prompt("\nPress ENTER to exit.")
+    }
+}
+
+try {
     exitCode = (await $`bun run build`).exitCode
 } finally {
     if (exitCode !== 0) {
